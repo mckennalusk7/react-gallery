4e35639387550e4d298e208f602248178ff9216d
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-12 20:09:30 -0600

    Gallerylist imported

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import GalleryList from "../GalleryList/GalleryList";
 
 class App extends Component {
+  componentDidMount() {
+    this.getImages();
+  }
+
+  getImages() {
+    axios
+      .get("gallery")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.warn(err));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <GalleryList />
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class GalleryList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default GalleryList;
